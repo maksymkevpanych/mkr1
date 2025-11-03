cef8be7574f83f913ece137d20ab5aebb9ec15d9
Max <dreamstore@MacBook-Pro-2.local>
2025-11-03 12:11:07 +0200

    2

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
